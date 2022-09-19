8ac9b009e6d9111cd128673a4e56ad59cd92291b
Toeffe3 <victor.jacobsen@outlook.dk>
2022-09-20 00:27:29 +0200

    Initial Svelte page

---
+++ src/lib/Socials.svelte
@@ -0,0 +1,70 @@
+<script>
+    // Get social logos
+    const socials = [
+        {
+            name: 'Github',
+            description: 'Looking to contribute?',
+            logo: 'https://github.githubassets.com/favicons/favicon-dark.svg',
+            url: 'https://github.com/bhelpful',
+        },
+        {
+            name: 'LinkedIn',
+            description: 'Connect with us',
+            logo: 'https://cdn.worldvectorlogo.com/logos/linkedin-icon-2.svg',
+            url: 'https://linkedin.com/company/bhelpful',
+        },
+    ];
+</script>
+
+<style>
+    h2 {
+        line-height: 0.3em;
+        margin: 0;
+    }
+
+    .list {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+
+    .card {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        margin: 1em;
+        padding: 1em;
+        border-radius: 1em;
+    }
+
+    .card a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    .logo {
+        height: 5em;
+        padding: 0 1.5em;
+        will-change: filter;
+    }
+
+</style>
+
+<div id="socials">
+    <h2>Find us on:</h2>
+    <div class="list">
+        {#each socials as social}
+            <div class="card">
+                <a href={social.url} target="_blank">
+                <p>{social.description}</p>
+                    <img src={social.logo} class="logo" alt="Visit" />
+                    <h3>{social.name}</h3>
+                </a>
+            </div>
+        {/each}
+    </div>
+</div>
